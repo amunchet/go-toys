aef4e69dc4e99d9551a966de1fa022cac2b45d33
Chester Enright <amunchet@gmail.com>
2020-05-17 16:30:20 -0500

    feat: added switch file

--- 07_switch.go
+++ 07_switch.go
@@ -15,4 +15,20 @@ func main() {
 	default:
 		fmt.Println("Afternoon!")
 	}
+
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool!")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Not sure what this type is %T\n", t)
+
+		}
+	}
+
+	whatAmI(true)
+	whatAmI(7)
+	whatAmI("hey")
 }
