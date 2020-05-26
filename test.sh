7890be3a8b562c51afb301c098dac7f5244b3020
Chet Enright <chet.enright@gmail.com>
2020-05-25 22:41:06 -0500

    chore: starting up test.sh This will eventually run tests to make sure all the source files actually run and exit properly

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Tests that all source files run in go
+
+# List all directories
+
+# Change to each 
+
+# In each, list all go files
+
+# Run go run on each and record the result
+
+# If we fail, then entire fails
