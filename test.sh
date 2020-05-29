d0be33764ce10ae3d5ecb9e171a5fe5a60f6b2b0
Chet Enright <chet.enright@gmail.com>
2020-05-29 00:19:56 -0500

    chore: updated github actions

--- test.sh
+++ test.sh
@@ -2,12 +2,17 @@
 
 # Tests that all source files run in go
 
-# List all directories
+DIRS=`ls -d */`
 
-# Change to each 
 
-# In each, list all go files
+for dir in $DIRS; do
+    for fname in $dir/*.go; do
+        echo "Starting $fname..."
+        go run $fname 
+        if [ $? -gt 0 ]; then
+            echo "FAIL - $fname"
+            exit 1;
+        fi
+    done
+done
 
-# Run go run on each and record the result
-
-# If we fail, then entire fails
